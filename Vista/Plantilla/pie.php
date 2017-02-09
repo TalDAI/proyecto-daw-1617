a53d2ebaccc35308700cae8b26a4e7538e0dcb00
izaldibar <cvb>
2017-02-09 09:35:21 +0100

    Prueba David 2
    Ser

--- Vista/Plantilla/pie.php
+++ Vista/Plantilla/pie.php
@@ -15,7 +15,7 @@
             <ul class="list-unstyled">
                 <li><h5>Creada por:</h5></li>
                 <li>
-                    <h6>Los alumnos de las promociones David de Desarrollo de Aplicaciones Web</h6>
+                    <h6>Los alumnos de las promociones Davidcfghb de Desarrollo de Aplicaciones Web</h6>
                 </li>
             </ul>
             <ul class="list-unstyled">
