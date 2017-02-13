6b595f7a9ffb2bb769ee11fb18f8cd4e60a36c81
elleche69 <2gdwes01@a224p01.arriagainfo.local>
2017-02-13 10:00:01 +0100

    Comitt
    Aaaaa

--- Vista/Plantilla/prueba.php
+++ Vista/Plantilla/prueba.php
@@ -8,7 +8,7 @@
 <script src="<?php echo parent::getUrlRaiz();?>/Vista/Plantilla/JS/validetta-v1.0.1-dist/validettaLang-es-ES.js"></script>
 <script src="<?php echo parent::getUrlRaiz();?>/Vista/Login/Funciones.js"></script>
 <script src="<?php echo parent::getUrlRaiz();?>/Vista/Produccion/Funciones.js"></script>
-<script src="<?php echo parent::getUrlRaiz();?>/Vista/Administracion/funciones.js"></script>
+<script src="<?php echo parent::getUrlRaiz();?>/Vista/Administracion/funciones2.js"></script>
 
 
 </body>
