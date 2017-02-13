4094b3ed3772ce1feb1ac8c5364517584265a5b9
iZaldibar <2gdwes02@a224p02.arriagainfo.local>
2017-02-13 09:49:27 +0100

    Comió Irene nuevo pie de prueba
    Sisi

---
+++ Vista/Plantilla/prueba.php
@@ -0,0 +1,15 @@
+
+
+<script src="<?php echo parent::getUrlRaiz();?>/Vista/Plantilla/JS/jquery-2.2.1.min.js"></script>
+<script src="<?php echo parent::getUrlRaiz();?>/Vista/Plantilla/JS/bootstrap.min.js"></script>
+<script src="<?php echo parent::getUrlRaiz();?>/Vista/Login/Funciones.js"></script>
+<script src="<?php echo parent::getUrlRaiz();?>/Vista/Plantilla/JS/jshash-2.2/md5-min.js"></script>
+<script src="<?php echo parent::getUrlRaiz();?>/Vista/Plantilla/JS/validetta-v1.0.1-dist/validetta.min.js"></script>
+<script src="<?php echo parent::getUrlRaiz();?>/Vista/Plantilla/JS/validetta-v1.0.1-dist/validettaLang-es-ES.js"></script>
+<script src="<?php echo parent::getUrlRaiz();?>/Vista/Login/Funciones.js"></script>
+<script src="<?php echo parent::getUrlRaiz();?>/Vista/Produccion/Funciones.js"></script>
+<script src="<?php echo parent::getUrlRaiz();?>/Vista/Administracion/funciones.js"></script>
+
+
+</body>
+</html>
