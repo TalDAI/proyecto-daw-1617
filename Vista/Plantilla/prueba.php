ff1dc0275f46556f7ee20576bfa120c6e3239e4c
elleche69 <ispas.decebalaitor@koldomitxelena.net>
2017-02-16 08:34:30 +0100

    comite prueba modificada
    eeeee

--- Vista/Plantilla/prueba.php
+++ Vista/Plantilla/prueba.php
@@ -4,11 +4,6 @@
 <script src="<?php echo parent::getUrlRaiz();?>/Vista/Plantilla/JS/bootstrap.min.js"></script>
 <script src="<?php echo parent::getUrlRaiz();?>/Vista/Login/Funciones.js"></script>
 <script src="<?php echo parent::getUrlRaiz();?>/Vista/Plantilla/JS/jshash-2.2/md5-min.js"></script>
-<script src="<?php echo parent::getUrlRaiz();?>/Vista/Plantilla/JS/validetta-v1.0.1-dist/validetta.min.js"></script>
-<script src="<?php echo parent::getUrlRaiz();?>/Vista/Plantilla/JS/validetta-v1.0.1-dist/validettaLang-es-ES.js"></script>
-<script src="<?php echo parent::getUrlRaiz();?>/Vista/Login/Funciones.js"></script>
-<script src="<?php echo parent::getUrlRaiz();?>/Vista/Produccion/Funciones.js"></script>
-<script src="<?php echo parent::getUrlRaiz();?>/Vista/Administracion/funciones2.js"></script>
 
 
 </body>
